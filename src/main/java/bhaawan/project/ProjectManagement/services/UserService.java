8f734102f5fb6be1491dcfbd3882af97d7995ecb
Bhaawan_Shrivastav <bhaawan272002@gmail.com>
2025-08-26 23:21:52 +0530

    made the password encrypted

--- src/main/java/bhaawan/project/ProjectManagement/services/UserService.java
+++ src/main/java/bhaawan/project/ProjectManagement/services/UserService.java
@@ -3,6 +3,8 @@ package bhaawan.project.ProjectManagement.services;
 import bhaawan.project.ProjectManagement.models.UserModel;
 import bhaawan.project.ProjectManagement.repositories.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,7 +15,11 @@ public class UserService {
     @Autowired
     private UserRepo userRepo;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public void registerUser(UserModel user){
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         userRepo.save(user);
     }
 
